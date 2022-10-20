d7c39800bde26b0d8b469ca311fa701144f10bf0
Steven Hawkins <Steven Hawkins@PTNOWSA6000378>
2022-10-20 15:56:41 +0100

    Removed template stories

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -12,7 +12,7 @@ export default function Alert(props: AlertProps) {
         className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
         role="alert"
       >
-        <strong className="font-bold">{label}!</strong>
+        <strong className="font-bold">{label}! </strong>
         <span className="block sm:inline">
           Something seriously bad happened.
         </span>
